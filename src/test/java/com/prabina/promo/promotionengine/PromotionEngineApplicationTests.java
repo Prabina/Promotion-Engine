4d6ed2674a2aa710f237c7e06b925f52bdd6ba52
Prabina Pani <prabinapani@Prabinas-MacBook-Pro.local>
2021-11-27 22:38:04 +0530

    Initial setup

---
+++ src/test/java/com/prabina/promo/promotionengine/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabina.promo.promotionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
