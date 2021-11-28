a8de85fc9275b2bb6f8d18e4e4f805f47aa667d5
Prabina Pani <prabinapani@Prabinas-MacBook-Pro.local>
2021-11-28 22:33:38 +0530

    Implement N items for fixed price promotion strategy

--- src/main/java/com/prabina/promo/promotionengine/promoEngine/FixedPriceForNItems.java
+++ src/main/java/com/prabina/promo/promotionengine/promoEngine/FixedPriceForNItems.java
@@ -1,12 +1,37 @@
 package com.prabina.promo.promotionengine.promoEngine;
 
 import com.prabina.promo.promotionengine.entities.CartItem;
+import com.prabina.promo.promotionengine.entities.Promotion;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Component
 public class FixedPriceForNItems implements PromoStrategy{
     @Override
     public BigDecimal applyPromo(CartItem cartItem) {
-        return null;
+        BigDecimal discount = BigDecimal.ZERO;
+
+        if (!cartItem.isPromoApplied()) {
+            Promotion promotion = cartItem.getProduct().getPromotion();
+
+            if (promotion.getNoOfItems() <= cartItem.getQuantity()) {
+
+                int noOfPromosApplied = cartItem.getQuantity() / promotion.getNoOfItems();
+
+                BigDecimal price = cartItem.getProduct().getPrice();
+                BigDecimal qty = new BigDecimal(noOfPromosApplied * promotion.getNoOfItems());
+
+                BigDecimal beforeDiscount = price.multiply(qty);
+
+                int discountedAmount = cartItem.getQuantity() / promotion.getNoOfItems() * promotion.getDiscount().intValue();
+
+                discount = beforeDiscount.subtract(new BigDecimal(discountedAmount));
+
+                cartItem.setPromoApplied(true);
+            }
+        }
+
+        return discount;
     }
 }
